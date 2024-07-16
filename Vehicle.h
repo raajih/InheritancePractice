6cf4b8071529606e34cb619b5e6a583c66beb21e
Raajih Roland <raajihroland88@gmail.com>
2024-07-16 09:33:32 -0400

    Finished with vehicle header

--- Vehicle.h
+++ Vehicle.h
@@ -1,5 +1,31 @@
-#pragma once
+//Raajih Roland
+//CIS 1202.5T1
+//July 16, 2024
+//SPECIFICATION FILE
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+
 class Vehicle
 {
+private:
+	//Member variables
+	string manufacturer;
+	int yearBuilt;
+
+public:
+	Vehicle();//Default constructor
+	Vehicle (string, int);//Constructor with paramters for manufacturer and year built
+	void setManufacturer(string);
+	void setYearBuilt(int);
+	string getManufacturer() const;
+	int getYearBuilt() const;
+	void displayInfo() const;
 };
 
+
+#endif
