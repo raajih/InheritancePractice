f71a0771f673773716104c7c232263c8e9c28966
Raajih Roland <raajihroland88@gmail.com>
2024-07-16 09:41:11 -0400

    Finished vehicle implementation file

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,1 +1,43 @@
+//Raajih Roland
+//CIS 1202.5T1
+//July 16, 2024
+//IMPLEMENTATION FILE
 #include "Vehicle.h"
+
+Vehicle::Vehicle()//Default constructor
+{
+	manufacturer = "";
+	yearBuilt = 0;
+}
+
+Vehicle::Vehicle(string make, int year)
+{
+	manufacturer = make;
+	yearBuilt = year;
+}
+
+void Vehicle::setManufacturer(string make)
+{
+	manufacturer = make;
+}
+
+void Vehicle::setYearBuilt(int year)
+{
+	yearBuilt = year;
+}
+
+string Vehicle::getManufacturer() const
+{
+	return manufacturer;
+}
+
+int Vehicle::getYearBuilt() const
+{
+	return yearBuilt;
+}
+
+void Vehicle::displayInfo() const
+{
+	cout << "Manufacturer: " << manufacturer
+		<< "\nYear built: " << yearBuilt;
+}
